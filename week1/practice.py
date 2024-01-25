b346010fa142c8a9abf7d4f0c821ddce06622b05
ejjem <tigonlee0627@gmail.com>
2024-01-25 17:00:18 +0900

    week1 코딩 테스트 제출

--- week1/practice.py
+++
@@ -1,3 +0,0 @@
-a = list(range(1, 11))                      # a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 생성
-result = [y*3 for y in a if y%2 == 0]       # a 내에서 x%2 == 0 인 x들만 3배 하라는 뜻
-print(result)                               # result 출력
